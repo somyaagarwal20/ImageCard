931993360a40fc387982a3fd58fc569301c6c275
somya <somyaagarwal3745@gmail.com>
2023-09-01 10:45:41 +0530

    file is added

--- src/DCompopnent.jsx
+++ src/DCompopnent.jsx
@@ -4,8 +4,6 @@ import DataContext from './DataContext'
 const DCompopnent = () => {
     const shareData = useContext(DataContext);
   return (
- 
-    
     <div>
       DComponent
       <p>property recived from grand parent:{shareData}
